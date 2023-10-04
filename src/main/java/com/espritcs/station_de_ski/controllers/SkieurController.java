dfbf48929d2edbcf439ac02bd850d2eee85bd777
mohamed sallami rhouma <mohamed-sr@hotmail.com>
2023-10-04 19:50:11 +0100

    set annotation RequiredArgsConstructor instead of Autowired

--- src/main/java/com/espritcs/station_de_ski/controllers/SkieurController.java
+++ src/main/java/com/espritcs/station_de_ski/controllers/SkieurController.java
@@ -2,15 +2,17 @@ package com.espritcs.station_de_ski.controllers;
 
 import com.espritcs.station_de_ski.entities.Skieur;
 import com.espritcs.station_de_ski.services.ISkieurService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class SkieurController {
-
-   @Autowired
+   @Qualifier("ISkieurServiceImpl")
    private ISkieurService skieurService;
 
    @PostMapping()
